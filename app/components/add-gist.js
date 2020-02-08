a949bc6b281f29839c672748c3421cf2c3ebd0c2
JennyJudova <jjudova@gmail.com>
2020-02-08 13:14:55 +0000

    add-gist added

---
+++ app/components/add-gist.js
@@ -0,0 +1,9 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
+export default class AddGistComponent extends Component {
+  @action
+  addFile(body, description, fileName) {
+    this.args.addGistInfo(body, description, fileName);
+  }
+}
