2158be39e4cfc936059dc605cb7f65c373ed8b95
JennyJudova <jjudova@gmail.com>
2020-02-08 21:45:43 +0000

    gist-add added and index.hbs added

--- app/components/add-gist.js
+++ app/components/add-gist.js
@@ -1,9 +1,13 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class AddGistComponent extends Component {
+  @service gistsInfo;
+
   @action
   addFile(body, description, fileName) {
-    this.args.addGistInfo(body, description, fileName);
+    this.gistsInfo.addGist({ body, description, fileName });
+    console.log(body, description, fileName);
   }
 }
